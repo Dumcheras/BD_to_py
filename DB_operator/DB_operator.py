27ae71eb2b0f7235158779ef8d2d0d797cf48ee4
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-10-17 15:28:18 +0300

    the method for creating a database has been filled in, the functionality for connecting and creating a database has been implemented

--- DB_operator/DB_operator.py
+++ DB_operator/DB_operator.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import SQL_Queries
 
 
-class ConnectDB: # подключение к бд
+class ConnectDB:  # подключение к бд
     @staticmethod
     def connect_to_db(server, database, user, password):
         ConnectionString = f'''DRIVER={{ODBC Driver 17 for SQL Server}};
@@ -26,5 +26,24 @@ class MSSQLOperator:
     def __init__(self, connector_obj):
         self.conn = connector_obj
 
-    def create_database(self, database_name, size=None, maxsize=None, filegrowth=None):    # метод создания бд
-        SQL_COMMAND= SQL_Queries
+    def create_database(self, database_name, size=None, maxsize=None, filegrowth=None):  # метод создания бд
+        SQL_COMMAND = SQL_Queries.create_database(database_name, size, maxsize, filegrowth)
+        try:
+            self.conn.execute(SQL_COMMAND)
+        except pyodbc.ProgrammingError as ex:
+            print(ex)
+        else:
+            return f'База данных {database_name} успешно создана'
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    SERVER = os.getenv('MS_SQL_SERVER')
+    DATABASE = os.getenv('MS_SQL_DATABASE')
+    USER = os.getenv('MS_SQL_USER')
+    PASSWORD = os.getenv('MS_SQL_KEY')
+    new_db = 'EmployersVacancies'
+
+    my_conn = ConnectDB.connect_to_db(SERVER, DATABASE, USER, PASSWORD)
+    my_db_operator = MSSQLOperator(my_conn)
+    print(my_db_operator.create_database(new_db, '10', '20', '5%'))
